1e21e4efaa589fb961bce3e451f15eed2c3f8224
mmartine2008 <mariano.andres.martinez@gmail.com>
2024-03-20 00:41:46 -0300

    Agrega gitignore

--- ej00/HelloWorld.java
+++ ej00/HelloWorld.java
@@ -2,9 +2,15 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-
-        System.out.println("Hola Mundo!"); 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
 
         
+        
+
     }
 }
